6e2b96c7f8cc33c3b5ba46101dc6e6704cd00c2d
MartinS <funkyspider@gmail.com>
2020-02-02 07:08:06 +0000

    Add project files.

---
+++ DXApplication3/ClassWriter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXApplication3
+{
+    public static class ClassWriter
+    {
+        static readonly List<string> ClassLines = new List<string>
+        {
+            "using System;",
+            "using System.Collections.Generic;",
+            "using System.Linq;",
+            "using System.Threading.Tasks;",
+            "using Microsoft.AspNet.OData;",
+            "using Microsoft.AspNetCore.Http;",
+            "using Microsoft.AspNetCore.Mvc;",
+            "using Microsoft.Extensions.Logging;",
+            "using Savant.Pulse.DataAccessLayer.ReferenceData;",
+            "using Savant.Pulse.DataAccessLayer.ReferenceData.Models;",
+            "",
+            "namespace Savant.Pulse.WebApi.ReferenceData.Controllers",
+            "{",
+            "    [Route(\"api/[controller]\")]",
+            "    [ApiController]",
+            "    public class %classname%Controller : ODataController",
+            "    {",
+            "",
+            "        private readonly PulseReferenceContext _context;",
+            "        private readonly ILogger<ReferenceDataApiController> _logger;",
+            "",
+            "        public %classname%Controller(",
+            "            PulseReferenceContext context,",
+            "            ILogger<ReferenceDataApiController> logger)",
+            "        {",
+            "            _context = context;",
+            "            _logger = logger;",
+            "        }",
+            "",
+            "        [HttpGet]",
+            "        [EnableQuery]",
+            "        public IQueryable<%classname%> Get()",
+            "        {",
+            "            var sp = _context.%classname%;",
+            "            return sp;",
+            "        }",
+            "    }",
+            "}"
+        };
+
+        public static void CreateClassForDbTable(string tableName)
+        {
+            var newClass = ClassLines.Select(s => s.Replace("%classname%", tableName)).ToList();
+            TextWriter tw = new StreamWriter($"c:\\temp\\{tableName}Controller.cs");
+
+            foreach (String s in newClass)
+                tw.WriteLine(s);
+
+            tw.Close();
+
+        }
+    }
+}
---
+++ DXApplication3/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Documents;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.POCO;
+using DevExpress.Mvvm.DataAnnotations;
+
+namespace DXApplication3.ViewModels
+{
+    [POCOViewModel]
+    public class MainViewModel : ViewModelBase
+    {
+        public IMessageBoxService MessageBoxService
+        {
+            get { return this.GetService<IMessageBoxService>(); }
+        }
+
+        public virtual string[] TableNames { get; set; }
+
+        public List<string> Tables = new List<string> { "Abocon", "Abotrn", "Addrclen" };
+        public virtual string Name { get; set; }
+
+        // We recommend that you do not use public constructors to prevent creating a View Model instance without the ViewModelSource 
+        public MainViewModel()
+        {
+            Name = "Martin";
+            foreach (var table in Tables)
+            {
+                ClassWriter.CreateClassForDbTable(table);
+            }
+            
+        }
+
+        public bool CanCreateRepos()
+        {
+            return TableNames.Length > 0;
+        }
+
+        public void CreateRepos()
+        {
+            MessageBoxService.Show("here");
+        }
+    }
+}
